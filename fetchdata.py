0cc5f3fbe74c14da702519400e4868ba93dd3be4
wendybazua <70781976+wendybazua@users.noreply.github.com>
2024-08-10 16:18:56 -0700

    Update fetchdata.py

--- fetchdata.py
+++ fetchdata.py
@@ -1,12 +1,1 @@
-import yfinance as yf
-import pandas as pd
-from loguru import logger
 
-def fetch_stock_data(tickers, start_date, end_date):
-    try:
-        data = yf.download(tickers, start=start_date, end=end_date, group_by='ticker')
-        data_clean = data.fillna(method='ffill')
-        return data_clean
-    except Exception as e:
-        logger.error(f"Error fetching data: {str(e)}")
-        return None
